2d901a4b27e47e3f98effc988017549adfa9f1cb
Shane  Ghiotto <sghiotto1@gmail.com>
2025-10-24 09:05:38 -0700

    test commit

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ class TestClass2:
     def print_test_str2(test_str2: str):
         print("Printing the test string now...")
         print(test_str2)
+
+class TestClass3:
+    def print_test_str2(test_str2: str):
+        print("Printing the test string now...")
+        print(test_str2)
